2a1924a7d56307d7eaa09b0dc4649a7106e76709
Bonjemus <59416055+Bonjemus@users.noreply.github.com>
2021-05-07 14:00:01 +0200

    Optimisations

---
+++ CustomDoorAccess/EventHandler.cs
@@ -0,0 +1,48 @@
+﻿using Synapse;
+using Synapse.Api.Events.SynapseEventArguments;
+using System.Linq;
+
+namespace CustomDoorAccess.Handlers
+{
+    public class EventHandler
+    {
+        public EventHandler()
+        {
+            Server.Get.Events.Player.PlayerGeneratorInteractEvent += OnGenerator;
+            Server.Get.Events.Map.DoorInteractEvent += OnDoor;
+        }
+
+        private void OnGenerator(PlayerGeneratorInteractEventArgs ev)
+        {
+            if (ev.Player == null || ev.Generator == null) return;
+
+            if (PluginClass.Configs.RolesWhichCanBypass != null && PluginClass.Configs.RolesWhichCanBypass.Contains(ev.Player.RoleType) || PluginClass.Configs.TeamsWhichCanBypass != null && PluginClass.Configs.TeamsWhichCanBypass.Contains(ev.Player.Team))
+                ev.Allow = true;
+
+            else if (PluginClass.Configs.AllowedItems != null && ((ev.Player.ItemInHand != null && PluginClass.Configs.AllowedItems.Contains(ev.Player.ItemInHand.ID)) || PluginClass.Configs.AllowedItems.Contains(-1)))
+                ev.Allow = true;
+
+            else if (PluginClass.Configs.replaceDefaultItemsPermissions)
+                ev.Allow = false;
+        }
+
+        private void OnDoor(DoorInteractEventArgs ev)
+        {
+            if (ev.Player == null || ev.Door == null) return;
+
+            PluginClass.CustomDoorPermission cdp = PluginClass.Configs.customDoorPermissionsList.FirstOrDefault(n => n.doorType == ev.Door.DoorType);
+            if (cdp == null) return;
+
+
+
+            else if (cdp.RolesWhichCanBypass != null && cdp.RolesWhichCanBypass.Contains(ev.Player.RoleType) || cdp.TeamsWhichCanBypass != null && cdp.TeamsWhichCanBypass.Contains(ev.Player.Team))
+                ev.Allow = true;
+
+            else if (cdp.AllowedItems != null && ((ev.Player.ItemInHand != null && cdp.AllowedItems.Contains(ev.Player.ItemInHand.ID)) || cdp.AllowedItems.Contains(-1)))
+                ev.Allow = true;
+
+            else if (cdp.replaceDefaultItemsPermissions)
+                ev.Allow = false;
+        }
+    }
+}
---
+++ PluginClass.cs
@@ -0,0 +1,36 @@
+﻿using Synapse.Api.Enum;
+using Synapse.Api.Plugin;
+using System.Collections.Generic;
+
+namespace CustomDoorAccess
+{
+    [PluginInformation(
+        Name = "Custom Door Access ",
+        Author = "Bonjemus",
+        Description = "A plug-in which allows to setup doors/generators permissions.",
+        LoadPriority = 1,
+        SynapseMajor = SynapseController.SynapseMajor,
+        SynapseMinor = SynapseController.SynapseMinor,
+        SynapsePatch = SynapseController.SynapsePatch,
+        Version = "1.1.0"
+        )]
+    public class PluginClass : AbstractPlugin
+    {
+        [Config(section = "Custom Door Access")]
+        public static Configs.Configs Configs;
+
+        public class CustomDoorPermission
+        {
+            public DoorType doorType;
+            public bool replaceDefaultItemsPermissions;
+            public List<int> AllowedItems;
+            public List<RoleType> RolesWhichCanBypass;
+            public List<Team> TeamsWhichCanBypass;
+        }
+
+        public override void Load()
+        {
+            new Handlers.EventHandler();
+        }
+    }
+}
